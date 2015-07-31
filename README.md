b9ba1bf8717f489afe9deee304d3fdcdead373c4
Dave Konopka <konopka@promptworks.com>
2015-07-31 15:14:42 -0400

    Flesh out readme

--- README.md
+++ README.md
@@ -4,12 +4,17 @@ Bootstrap a Chef Server from scratch.
 
 ## Components
 
-* Bootstrap bash script
-* Vagrant test harness
-* Troposphere generated CloudFormation template
+* `bootstrap-chef.sh` Chef server bootstrap bash script.
+* `Vargantfile` Vagrant test harness for bootstrap script.
+* `chef-server.py` [Troposphere](https://github.com/cloudtools/troposphere) CloudFormation template for launching an EC2 instance.
+* `chef-server.json` Generated CloudFormation json.
 
-### Project goals
+## Commands
 
-* [x] Create a fresh EC2 instance with associated resources.
-* [x] Bootstrap with Chef server software.
-* [ ] Copy generated credential files to an S3 bucket.
+### Generate CloudFormation json
+
+* `python chef-server.py > chef-server.json`
+
+### Launch CloudFormation stack
+
+* `aws cloudformation create-stack --stack-name chef-server --template-body file://./chef-server.json --parameters ParameterKey=KeyName,ParameterValue=somekey`
--- chef-server.py
+++ chef-server.py
@@ -1,13 +1,40 @@
 import troposphere.ec2 as ec2
-from troposphere import Base64, Ref, Template
+from troposphere import Base64, FindInMap, Parameter, Ref, Template
 
 template = Template()
+
+# Mappings
+
+# Ubuntu Trusty 64 bit HVM
+template.add_mapping("RegionMap", {
+    "us-east-1": {"AMI": "ami-7be63d10"}
+})
+
+# Parameters
+keyname_param = template.add_parameter(Parameter(
+    "KeyName",
+    Type="String",
+    Description="Name of an existing EC2 KeyPair to "
+                "enable SSH access to the instance",
+))
+
+template.add_parameter(Parameter(
+    "InstanceType",
+    Type="String",
+    Description="EC2 instance type",
+    Default="m3.medium",
+    AllowedValues=[
+        "m3.medium", "m3.large", "m3.xlarge", "m3.2xlarge",
+        "c1.medium", "c1.xlarge", "cc1.4xlarge", "cc2.8xlarge", "cg1.4xlarge"
+    ],
+    ConstraintDescription="Must be a valid, EC2 classic compatible instance type.",
+))
 
 # Define the instance security group
 instance_sg = template.add_resource(
     ec2.SecurityGroup(
         "InstanceSecurityGroup",
-        GroupDescription="Enable SSH and HTTP access on the inbound port",
+        GroupDescription="Enable SSH access to the world",
         SecurityGroupIngress=[
             ec2.SecurityGroupRule(
                 IpProtocol="tcp",
@@ -25,10 +52,10 @@ cloud_init = cloud_init_file.read()
 cloud_init_file.close()
 
 ec2_instance = template.add_resource(ec2.Instance(
-    "Ec2Instance",
-    ImageId="ami-7be63d10",
-    KeyName="experiment_dkonopka.pem",
-    InstanceType="m3.medium",
+    "ChefServerInstance",
+    ImageId=FindInMap("RegionMap", Ref("AWS::Region"), "AMI"),
+    KeyName=Ref(keyname_param),
+    InstanceType=Ref("InstanceType"),
     SecurityGroups=[Ref(instance_sg)],
     UserData=Base64(cloud_init)
 ))
